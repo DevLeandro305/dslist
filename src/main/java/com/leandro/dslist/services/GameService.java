1b797cf200f89f2e43dadb00cbd9437eabe1a52d
Leandro Soares <lfsoares.dev@gmail.com>
2024-11-17 11:52:17 -0300

    Consulta SQL, projection, busca de games por lista

--- src/main/java/com/leandro/dslist/services/GameService.java
+++ src/main/java/com/leandro/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.leandro.dslist.services;
 import com.leandro.dslist.dto.GameDTO;
 import com.leandro.dslist.dto.GameMinDTO;
 import com.leandro.dslist.entities.Game;
+import com.leandro.dslist.projections.GameMinProjection;
 import com.leandro.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,5 +28,11 @@ public class GameService {
         Game result = gameRepository.findById(id).get();
         GameDTO dto = new GameDTO(result);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(GameMinDTO::new).toList();
     }
 }
